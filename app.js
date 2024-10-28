4842cf1e6c1672f5f7c6f1e06356529cf672a6f9
Kazim Hasani <smkazimhasani@gmail.com>
2024-10-28 08:40:57 -0700

    Edit & Update Route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride = require("method-override");
 
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozynest";
@@ -21,6 +22,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //Home Route
 app.get("/", (req, res) => {
@@ -50,6 +52,20 @@ app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
 app.listen(port, () => {
