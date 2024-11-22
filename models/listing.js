c19af48172778870c2eda075bf767049d93983b4
Kazim Hasani <smkazimhasani@gmail.com>
2024-11-22 07:18:12 +0500

    Success & Failure Text

--- models/listing.js
+++ models/listing.js
@@ -8,13 +8,18 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o",
-    set: (v) =>
-      v === ""
-        ? "https://unsplash.com/photos/a-house-with-a-blue-front-door-and-a-brown-front-door-xaqsFfoEq3o"
-        : v,
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v,
+    },
+    filename: {
+      type: String,
+    },
   },
   price: Number,
   location: String,
