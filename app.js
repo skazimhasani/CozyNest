f83fd1a920dc4d967c79a360c878781f32281886
Kazim Hasani <smkazimhasani@gmail.com>
2024-11-25 07:54:22 +0500

    Error.ejs

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.all("*", (req, res, next) => {
 //Error Middleware
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(port, () => {
