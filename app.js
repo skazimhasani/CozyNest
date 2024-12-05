cf76bc8683b4e50c23320d02105a4693d18b8f9f
Kazim Hasani <smkazimhasani@gmail.com>
2024-12-05 16:33:43 +0500

    Restructuring Reviews

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozynest";
 
-const Listing = require("./models/listing.js");
-const Review = require("./models/review.js");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 main()
   .then(() => {
@@ -33,16 +30,6 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
 //Home Route
 app.get("/", (req, res) => {
   res.send("Hi, I am Root");
@@ -50,35 +37,8 @@ app.get("/", (req, res) => {
 
 //Listings
 app.use("/listings", listings);
-
-//Review (POST Route)
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-
-//Review (DELETE Route)
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-  })
-);
+//Reviews
+app.use("/listings/:id/reviews", reviews);
 
 //All not available Routes
 app.all("*", (req, res, next) => {
