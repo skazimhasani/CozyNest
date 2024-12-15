cf6e93ed7851a05f9d3fe48b6b0120f3c34a89b6
Kazim Hasani <smkazimhasani@gmail.com>
2024-12-15 16:30:43 +0500

    User Login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -26,4 +27,19 @@ router.post(
   })
 );
 
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to CozyNest!");
+    res.redirect("/listings");
+  }
+);
 module.exports = router;
