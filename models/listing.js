20e3bb69b7a05acbd6cb873c17392a92b1daf492
Kazim Hasani <smkazimhasani@gmail.com>
2024-12-18 22:35:36 +0500

    Image Display

--- models/listing.js
+++ models/listing.js
@@ -9,18 +9,8 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    url: {
-      type: String,
-      default:
-        "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) =>
-        v === ""
-          ? "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          : v,
-    },
-    filename: {
-      type: String,
-    },
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
