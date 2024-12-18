9c52358f361cbf37d2735b2407cb8826dc2e7d17
Kazim Hasani <smkazimhasani@gmail.com>
2024-12-18 15:16:38 +0500

    Multer

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
 router
   .route("/")
   .get(wrapAsync(listingController.index))
